2018ec95ee452761842c50cf6373811e4f71c997
OmiHernandez <hernandez.romo.n14@gmail.com>
2024-03-08 18:54:00 -0600

    canvas

---
+++ js/script-canvas.js
@@ -0,0 +1,42 @@
+var canvas = document.getElementById('lienzoP');
+canvas.width = 900;
+canvas.height = 550;
+var ctx = canvas.getContext('2d');
+
+ctx.font = 'bold 48px Arial';
+ctx.fillStyle = '#20234d';
+ctx.textAlign = 'center';
+ctx.fillText('Pokemon OWO', canvas.width / 2, 100);
+
+
+function drawButton(text, x, y, width, height) {
+    ctx.fillStyle = '#4CAF50';
+    ctx.fillRect(x, y, width, height);
+
+    ctx.roundRect(x, y, width, height, 10);
+    ctx.font = 'bold 24px Arial';
+    ctx.fillStyle = 'white';
+    ctx.textAlign = 'center';
+    
+
+    ctx.fillText(text, x + width / 2, y + height / 2 + 10);
+}
+
+canvas.addEventListener('click', function(event) {
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+
+    if (mouseX > 100 && mouseX < 300 && mouseY > 450 && mouseY < 500) {
+      alert('¡Has hecho clic en Jugar!');
+    } else if (mouseX > 350 && mouseX < 550 && mouseY > 450 && mouseY < 500) {
+      alert('¡Has hecho clic en Instrucciones!');
+    } else if (mouseX > 600 && mouseX < 800 && mouseY > 450 && mouseY < 500) {
+      alert('¡Has hecho clic en Puntaje!');
+    }
+});
+
+  
+drawButton('Jugar', 100, 450, 200, 50);
+drawButton('Instrucciones', 350, 450, 200, 50);
+drawButton('Tu Puntaje', 600, 450, 200, 50);
