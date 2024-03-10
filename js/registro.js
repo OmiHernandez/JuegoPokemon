ce80291417121f62e32287d56c45b7b543f7f587
WallaceGaster <saitama.kurosakyy@gmail.com>
2024-03-09 21:01:32 -0600

    Veloz-tabla de puntuaciones correcciones

--- js/registro.js
+++ js/registro.js
@@ -38,13 +38,6 @@ function tocarMusica() {
     }
 }
 
-function repetirMusica() {
-    audio.currentTime = 0;
-    audio.play();
-}
-
-audio.addEventListener('ended', repetirMusica);
-
 musica.addEventListener('click', tocarMusica);
 
 function drawButton(text, x, y, width, height) {
@@ -72,6 +65,7 @@ function drawButton(text, x, y, width, height) {
         ctx.fillText(text, x + width / 2, y + height / 2 + 10);
 
         if (mouseX > 350 && mouseX < 550 && mouseY > 450 && mouseY < 500) {
+            //Instrucciones
             ctx.fillStyle = '#f89807';
             ctx.fillRect(350, 450, 200, 50);
 
@@ -106,60 +100,10 @@ drawButton('Comenzar', 350, 450, 200, 50);
 //****************************************************
 //**********GUARDAR EN EL LOCALSTORAGE**************** 
 //****************************************************
-// function guardar() {
-//     var nombre = document.getElementById('nombre').value;
-//     localStorage.setItem('nombre', nombre);
-//     // window.open("../juego.html");
-//     window.location.href = 'juego.html';
-
-// }
-
 function guardar() {
     var nombre = document.getElementById('nombre').value;
+    localStorage.setItem('nombre', nombre);
+    // window.open("../juego.html");
+    window.location.href = 'juego.html';
 
-    function verificarNombre(nombre) {
-        return localStorage.getItem(nombre) !== null;
-    }
-
-    function almacenarNombre(nombre) {
-        if (!verificarNombre(nombre)) {
-            const datos = {
-                puntos: 0,
-                mejorTiempo: 0
-            };
-            localStorage.setItem(nombre, JSON.stringify(datos));
-            return true;
-        }
-        return false;
-    }
-
-    function obtenerDatos(nombre) {
-        return JSON.parse(localStorage.getItem(nombre));
-    }
-
-    if (nombre) {
-        const almacenado = almacenarNombre(nombre);
-        if (almacenado) {
-            // alert(`Nombre almacenado correctamente.${nombre}!`);
-            window.location.href = 'juego.html';
-        } else {
-            // alert(`El nombre "${nombre}" ya existe.`);
-            const datos = obtenerDatos(nombre);
-            Swal.fire({
-                imageUrl: '../img/pokebola.png',
-                imageWidth: 100,
-                imageHeight: 100,
-                title: `Bienvenido de nuevo ${nombre}`,
-                html: `Tus mayor puntuación es: <strong>${datos.puntos}</strong><br>Tu mejor tiempo es: <strong>${datos.mejorTiempo}</strong>`,
-                confirmButtonClass: 'record',
-                confirmButtonColor: '#f8ca07',
-                confirmButtonText: '¡A romper el record!'
-            }).then(() => {
-                window.location.href = 'juego.html';
-            });
-        }
-        // const datos = obtenerDatos(nombre);
-        // alert(`Tus puntos son: ${datos.puntos} y tu mejor tiempo es: ${datos.mejorTiempo}`);
-        // window.location.href = 'juego.html';
-    }
 }
