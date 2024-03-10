8f95555dc7dd55a91e458059a3be3fd9b7f8ff7c
OmiHernandez <hernandez.romo.n14@gmail.com>
2024-03-09 18:46:52 -0600

    mostrar puntaje

--- js/registro.js
+++ js/registro.js
@@ -72,7 +72,6 @@ function drawButton(text, x, y, width, height) {
         ctx.fillText(text, x + width / 2, y + height / 2 + 10);
 
         if (mouseX > 350 && mouseX < 550 && mouseY > 450 && mouseY < 500) {
-            //Instrucciones
             ctx.fillStyle = '#f89807';
             ctx.fillRect(350, 450, 200, 50);
 
@@ -141,12 +140,26 @@ function guardar() {
     if (nombre) {
         const almacenado = almacenarNombre(nombre);
         if (almacenado) {
-            alert(`Nombre almacenado correctamente. ¡Bienvenido ${nombre}!`);
+            // alert(`Nombre almacenado correctamente.${nombre}!`);
+            window.location.href = 'juego.html';
         } else {
-            alert(`El nombre "${nombre}" ya existe.`);
+            // alert(`El nombre "${nombre}" ya existe.`);
+            const datos = obtenerDatos(nombre);
+            Swal.fire({
+                imageUrl: '../img/pokebola.png',
+                imageWidth: 100,
+                imageHeight: 100,
+                title: `Bienvenido de nuevo ${nombre}`,
+                html: `Tus mayor puntuación es: <strong>${datos.puntos}</strong><br>Tu mejor tiempo es: <strong>${datos.mejorTiempo}</strong>`,
+                confirmButtonClass: 'record',
+                confirmButtonColor: '#f8ca07',
+                confirmButtonText: '¡A romper el record!'
+            }).then(() => {
+                window.location.href = 'juego.html';
+            });
         }
-        const datos = obtenerDatos(nombre);
-        alert(`Tus puntos son: ${datos.puntos} y tu mejor tiempo es: ${datos.mejorTiempo}`);
-        window.location.href = 'juego.html';
+        // const datos = obtenerDatos(nombre);
+        // alert(`Tus puntos son: ${datos.puntos} y tu mejor tiempo es: ${datos.mejorTiempo}`);
+        // window.location.href = 'juego.html';
     }
 }
